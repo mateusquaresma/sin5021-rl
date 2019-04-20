5260d116101a84ddd01aaf64237b0739c7b98538
Mateus Quaresma <mateus.quaresma@protonmail.com>
2019-04-20 17:59:12 -0300

    Added the ability to read any environment setup

--- src/env_reader.py
+++ src/env_reader.py
@@ -1,11 +1,11 @@
 import csv
-import pprint
+# import pprint
 
 
-def read_env_file(action_number_list):
+def read_env_file(env_number, action_number_list):
     T = {}
     for action_number in action_number_list:
-        with open('../resources/Ambiente1/Action0%s.txt' % (action_number, )) as csv_file:
+        with open('../resources/Ambiente%s/Action0%s.txt' % (env_number, action_number)) as csv_file:
             mdp_env = csv.reader(csv_file, delimiter=' ')
             for row in mdp_env:
                 s, sp, pr = tuple([float(data) for data in row if len(data) > 0])
@@ -19,6 +19,6 @@ def read_env_file(action_number_list):
     return T
 
 
-# transition_matrix = read_env_file(list(range(1, 7)))
+# transition_matrix = read_env_file(1, list(range(1, 7)))
 # pp = pprint.PrettyPrinter(indent=3)
 # pp.pprint(transition_matrix)
